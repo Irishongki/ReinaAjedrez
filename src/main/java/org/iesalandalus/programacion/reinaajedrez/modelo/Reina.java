1ca56cfcaa829d2a1b11b8f9a41f7d9a5986acae
Irene <Irene@192.168.10.33>
2022-11-15 14:00:40 +0100

    Crear los métodos get y set con excepciones

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -5,4 +5,24 @@ public class Reina {
 	private Color color;
 	private Posicion posicion;
 
+	public Color getColor() {
+		return color;
+	}
+
+	private void setColor(Color color) {
+		if (color == null) {
+			throw new NullPointerException("ERROR: El color no puede ser nulo.");
+		}
+		this.color = color;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	private void setPosicion(Posicion posicion) {
+
+		this.posicion = new Posicion(posicion);
+	}
+
 }
