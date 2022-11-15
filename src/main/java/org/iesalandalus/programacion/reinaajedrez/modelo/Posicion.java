cb2145d7f6358d6bee57dd11b91b2d9e6974b91f
Irene <Irene@192.168.10.33>
2022-11-15 13:18:07 +0100

    Crear los métodos get y set con sus excepciones

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -1,7 +1,33 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
+import java.util.Objects;
+
+import javax.naming.OperationNotSupportedException;
+
 public class Posicion {
 	private int fila;
 	private char columna;
 
+	private void setFila(int fila) {
+		if (fila < 1 | fila > 8) {
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		}
+		this.fila = fila;
+	}
+
+	public int getFila() {
+		return fila;
+	}
+
+	private void setColumna(char columna) {
+		if (columna < 'a' | columna > 'h') {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		}
+		this.columna = columna;
+	}
+
+	public char getColumna() {
+		return columna;
+	}
 }
+	
