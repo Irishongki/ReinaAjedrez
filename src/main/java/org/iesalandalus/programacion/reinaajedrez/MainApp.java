eecf4452dc1bcc1b4a4867e81855fba4fa4b6cf6
Irene <Irene@DESKTOP-19IBV0R.technicolor.net>
2022-11-16 21:48:49 +0100

    Creamos el método ejecutarOpcion

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -6,6 +6,22 @@ public class MainApp {
 
 	private Reina reina;
 
+	private static void ejecutarOpcion(int opcion) {
+		switch (opcion) {
+		case 1:
+			break;// Crear reina por defecto
+		case 2:
+			break;// Crear reina eligiendo el color
+		case 3:
+			break;// Mover
+		case 4:
+			Consola.despedirse();
+			break;// Salir
+
+		}
+
+	}
+
 	public static void main(String[] args) {
 
 	}
