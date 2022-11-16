6006977c0a594e0b9b1bf8b3a4906cdd0f91a360
Irene <Irene@DESKTOP-19IBV0R.technicolor.net>
2022-11-16 23:08:57 +0100

    Creamos el método crearReinaDefecto()

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -4,7 +4,7 @@ import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
 
 public class MainApp {
 
-	private Reina reina;
+	private static Reina reina;
 
 	private static void ejecutarOpcion(int opcion) {
 		switch (opcion) {
@@ -17,9 +17,11 @@ public class MainApp {
 		case 4:
 			Consola.despedirse();
 			break;// Salir
-
 		}
+	}
 
+	private static void crearReinaDefecto() {
+		reina = new Reina();
 	}
 
 	public static void main(String[] args) {
