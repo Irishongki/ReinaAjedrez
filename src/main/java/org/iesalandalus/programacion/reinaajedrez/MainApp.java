4fdcca42801a48c740d44d65b517056f1abb85dc
Irene <Irene@192.168.10.33>
2022-11-17 08:45:46 +0100

    Crear el método mover() y añadirlo al método ejecutarOpcion()

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
 import org.iesalandalus.programacion.reinaajedrez.modelo.Color;
 import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
 
@@ -16,6 +18,7 @@ public class MainApp {
 			crearReinaColor();
 			break;// Crear reina eligiendo el color
 		case 3:
+			mover();
 			break;// Mover
 		case 4:
 			Consola.despedirse();
@@ -30,6 +33,15 @@ public class MainApp {
 	private static void crearReinaColor() {
 		reina = new Reina(Consola.elegirColor());
 
+	}
+
+	private static void mover() {
+		Consola.mostrarMenuDirecciones();
+		try {
+			reina.mover(Consola.elegirDireccion(), Consola.elegirPasos());
+		} catch (OperationNotSupportedException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 	public static void main(String[] args) {
