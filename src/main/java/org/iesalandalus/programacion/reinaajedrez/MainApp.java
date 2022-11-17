108b36a24a4d9c1de37b692063dc8f251fcf303e
Irene <Irene@192.168.10.33>
2022-11-17 08:36:56 +0100

    Crear el método crearReinaColor() y añadirlos al método ejecutarOpcion

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,5 +1,6 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.reinaajedrez.modelo.Color;
 import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
 
 public class MainApp {
@@ -9,8 +10,10 @@ public class MainApp {
 	private static void ejecutarOpcion(int opcion) {
 		switch (opcion) {
 		case 1:
+			crearReinaDefecto();
 			break;// Crear reina por defecto
 		case 2:
+			crearReinaColor();
 			break;// Crear reina eligiendo el color
 		case 3:
 			break;// Mover
@@ -22,6 +25,11 @@ public class MainApp {
 
 	private static void crearReinaDefecto() {
 		reina = new Reina();
+	}
+
+	private static void crearReinaColor() {
+		reina = new Reina(Consola.elegirColor());
+
 	}
 
 	public static void main(String[] args) {
